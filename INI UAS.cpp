be9c4815395956b63e2dde6a85b55d126598524e
Azra'i Mahendra <113901146+azrmhen@users.noreply.github.com>
2023-07-05 13:09:57 +0700

    Daspro Update (Hendra).cpp

--- INI UAS.cpp
+++ INI UAS.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 using namespace std;
+int i,j;
 struct Data{
 	string tujuan;
 	int harga;
@@ -14,12 +15,14 @@ void tampilanawal(){
 		<<"               TRAVELAIR               "<<endl
 		<<"======================================="<<endl
 		<<"1. Tiket List"<<endl
+		<<"2. Data urut Terendah - Tertinggi"<<endl
+		<<"3. Tiket Yang Tersedia"<<endl
+		<<"4. Total Harga"<<endl
 		<<"pilih menu : ";
 }
-
 void Tiket(Data temp[], int jml) {
     cout << "Tiket yang Tersedia" <<endl;
-    for (int i = 0; i < jml; i++) {
+    for (i = 0; i < jml; i++) {
        if (temp[i].status == "tersedia"){
             cout<<"Maskapai : "<<temp[i].pesawat<<endl;
             cout<<"Kelas : "<<temp[i].kelas<<endl;
@@ -29,16 +32,45 @@ void Tiket(Data temp[], int jml) {
       }cout<<endl;
     }
 }
+void SeluruhData(Data temp[], int jml){
+	cout<<"Urutan tiket dari terendah - tertinggi"<<endl;
+	for (i = 0; i < jml; i++) {
+		cout<<"Maskapai : "<<temp[i].pesawat<<endl;
+        cout<<"Kelas : "<<temp[i].kelas<<endl;
+        cout<<"Tujuan : "<<temp[i].tujuan<<endl;
+        cout<<"Status : "<<temp[i].status<<endl;
+        cout<<"Harga : "<<temp[i].harga<<endl;  
+	}
+}
+void DataUrut(Data tiket[], int jml){
+	int i,j;
+ 	i = jml-1;
+ 	do{
+ 		for(j=0;j<i;j++){
+ 			if(tiket[j+1].harga < tiket[j].harga)
+ 				swap(tiket[j+1],tiket[j]);
+			}
+ 	i--;
+ 	}while(i>0);
+}
+void swap(int &a, int &b){
+	int tmp;
+ 	tmp = a;
+ 	a = b;
+ 	b = tmp;
+}
+
 int main(){
 	int pilih;
 	char ulang;
-	const int jml = 5;
+	const int jml = 10;
     Data tiket[jml] = {
         {"Jakarta - Surabaya", 100000, "tersedia", "Ekonomi", "ATR"},
-        {"Surabaya - kalimantan", 150000, "tersedia", "Ekslusive", "AIRLANGGA"},
-        {"Kalimantan - Bandung", 120000, "tersedia", "Ekonomi", "LION AIR"},
+        {"Surabaya - Semarang", 150000, "tersedia", "Ekslusive", "AIRLANGGA"},
+        {"solo - Bandung", 120000, "tersedia", "Ekonomi", "LION AIR"},
         {"Medan - Yogyakarta", 80000, "tersedia", "Ekonomi", "AIRBUS"},
-        {"Makasar - Bali", 200000, "tersedia", "Ekonomi", "BOEING"}
+        {"Makasar - Bali", 200000, "tersedia", "Ekonomi", "BOEING"},
+        {"Kediri - Yogyakarta", 150000, "tersedia", "Ekonomi", "AIRLANGGA"}
     };
     
     //
@@ -52,6 +84,11 @@ int main(){
     			Tiket(tiket, jml);
     			cout<<"home y/n : ";
     			cin>>ulang;
+    		break;
+    		case 2:
+    			system("cls");
+    			DataUrut(tiket, jml);
+    			Tiket(tiket, jml);
     		break;
 		}
 	}while(ulang == 'y');
