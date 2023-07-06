7b9587cdc0aa75f4282b6939305b4a988c5107b2
Ahmad Muharram Alfarisi <137743169+Fariz08@users.noreply.github.com>
2023-07-06 12:55:50 +0700

    UAS Daspro bagian awal (Faris).cpp

--- INI UAS.cpp
+++ INI UAS.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 struct Data {
@@ -20,20 +19,9 @@ void tampilanawal() {
          << "4. Pesan Tiket\n"
          << "pilih menu: ";
 }
-void Tiket(Data temp[], int jml) {
-    cout << "Tiket yang Tersedia\n";
-    for (int i = 0; i < jml; i++) {
-        if (temp[i].status == "tersedia") {
-            cout << "Maskapai: " << temp[i].pesawat << endl;
-            cout << "Kelas: " << temp[i].kelas << endl;
-            cout << "Tujuan: " << temp[i].tujuan << endl;
-            cout << "Status: " << temp[i].status << endl;
-            cout << "Harga: " << temp[i].harga << endl << endl;;
-        }
-    }
-}
 
 void SeluruhData(Data temp[], int jml) {
+    cout << "Tiket List\n";
     for (int i = 0; i < jml; i++) {
         cout << "Maskapai: " << temp[i].pesawat << endl;
         cout << "Kelas: " << temp[i].kelas << endl;
@@ -43,21 +31,6 @@ void SeluruhData(Data temp[], int jml) {
     }cout<<endl;
 }
 
-void DataUrut(Data tiket[], int jml) {
-    for (int i = 0; i < jml - 1; i++) {
-        for (int j = 0; j < jml - i - 1; j++) {
-            if (tiket[j + 1].harga < tiket[j].harga) {
-                swap(tiket[j + 1], tiket[j]);
-            }
-        }
-    }
-}
-
-void swap(Data& a, Data& b) {
-    Data tmp = a;
-    a = b;
-    b = tmp;
-}
 
 int main() {
     int pilih;
@@ -67,9 +40,9 @@ int main() {
         {"Jakarta - Surabaya", 1200000, "tersedia", "Ekonomi", "CITILINK"},
         {"Surabaya - Semarang", 2500000, "tersedia", "Ekslusive", "GARUDA INDONESIA"},
         {"Solo - Bandung", 1400000, "tersedia", "Ekonomi", "LION AIR"},
-        {"Medan - Yogyakarta", 2300000, "tidak tersedia", "Ekslusive", "WINGS AIR"},
+        {"Medan - Yogyakarta", 2300000, "tersedia", "Ekslusive", "WINGS AIR"},
         {"Makassar - Bali", 2000000, "tersedia", "Ekonomi", "AIR ASIA"},
-        {"Kediri - Yogyakarta", 1800000, "tidak tersedia", "Ekonomi", "SRIWIJAYA AIR"}
+        {"Kediri - Yogyakarta", 1800000, "tersedia", "Ekonomi", "SRIWIJAYA AIR"}
     };
 
     do {
@@ -81,57 +54,7 @@ int main() {
                 system("cls");
                 SeluruhData(tiket, jml);
                 break;
-            case 2:
-                system("cls");
-                DataUrut(tiket, jml);
-                SeluruhData(tiket, jml);
-                break;
-            case 3:
-            	system("cls");
-                Tiket(tiket, jml);
-                break;
-            case 4: {
-                string tujuan;
-                int jumlah;
-                bool tiketTersedia = false;
-
-                cout << "Masukkan tujuan penerbangan: ";
-                cin.ignore();
-                getline(cin, tujuan);
-					
-                cout << "Masukkan jumlah tiket yang dipesan: ";
-                cin >> jumlah;
-				system("cls");
-                for (int i = 0; i < jml; i++) {
-                    if (tiket[i].tujuan == tujuan && tiket[i].status == "tersedia") {
-                        tiketTersedia = true;
-                        cout << "==========================================="<<endl;
-                        cout << "       Pemesanan tiket Anda berhasil!      " << endl;
-                        cout << "=========================================== " << endl;
-
-                        cout << "---------------------------------" << endl;
-                        cout << "Detail tiket yang sudah dipesan: " << endl;
-                        cout << "--------------------------------- " << endl;
-                        cout << "Maskapai: " << tiket[i].pesawat << endl;
-                        cout << "Kelas: " << tiket[i].kelas << endl;
-                        cout << "Tujuan: " << tiket[i].tujuan << endl;
-                        cout << "Harga per tiket: " << tiket[i].harga << endl;
-                        cout << "Jumlah tiket yang dipesan: " << jumlah << endl;
-                        cout << "Total harga: " << tiket[i].harga * jumlah << endl;
-                        break;
-                    }
-                }
-
-                if (!tiketTersedia) {
-                    cout << "Tiket tidak tersedia untuk tujuan tersebut." << endl;
-                }
-                break;
-            }
-            default:
-                cout << "Pilihan tidak valid." << endl;
-                break;
-        }
-
+		}
         cout << "Apakah Anda ingin melanjutkan (y/n)? ";
         cin >> ulang;
     } while (ulang == 'y' || ulang == 'Y');
